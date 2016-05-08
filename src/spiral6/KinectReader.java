8f5234112aaa57869f1fc520b3ae75f756762d9a
Shamee Mahmud <sami_safin_mahmud@hotmail.com>
2016-05-08 16:05:03 -0500

    Housekeeping.

--- src/spiral6/KinectReader.java
+++ src/spiral6/KinectReader.java
@@ -6,16 +6,17 @@ public class KinectReader {
 
 	public static void AngleCalculator(Skeleton s){
 		
-		/*System.out.println("Left shoulder: " + skeletons[i].get3DJointX(Skeleton.SHOULDER_LEFT) + "," + skeletons[i].get3DJointY(Skeleton.SHOULDER_LEFT) + "," + skeletons[i].get3DJointZ(Skeleton.SHOULDER_LEFT));
-		System.out.println("Left elbow: " + skeletons[i].get3DJointX(Skeleton.ELBOW_LEFT) + "," + skeletons[i].get3DJointY(Skeleton.ELBOW_LEFT) + "," + skeletons[i].get3DJointZ(Skeleton.ELBOW_LEFT));
-		System.out.println("Left wrist: " + skeletons[i].get3DJointX(Skeleton.WRIST_LEFT) + "," + skeletons[i].get3DJointY(Skeleton.WRIST_LEFT) + "," + skeletons[i].get3DJointZ(Skeleton.WRIST_LEFT));
-		System.out.println();
+		/*
+		 * System.out.println("Left shoulder: " + skeletons[i].get3DJointX(Skeleton.SHOULDER_LEFT) + "," + skeletons[i].get3DJointY(Skeleton.SHOULDER_LEFT) + "," + skeletons[i].get3DJointZ(Skeleton.SHOULDER_LEFT));
+		 * System.out.println("Left elbow: " + skeletons[i].get3DJointX(Skeleton.ELBOW_LEFT) + "," + skeletons[i].get3DJointY(Skeleton.ELBOW_LEFT) + "," + skeletons[i].get3DJointZ(Skeleton.ELBOW_LEFT));
+		 * System.out.println("Left wrist: " + skeletons[i].get3DJointX(Skeleton.WRIST_LEFT) + "," + skeletons[i].get3DJointY(Skeleton.WRIST_LEFT) + "," + skeletons[i].get3DJointZ(Skeleton.WRIST_LEFT));
+		 * System.out.println();
 		
-		System.out.println("Right shoulder: " + skeletons[i].get3DJointX(Skeleton.SHOULDER_RIGHT) + "," + skeletons[i].get3DJointY(Skeleton.SHOULDER_RIGHT) + "," + skeletons[i].get3DJointZ(Skeleton.SHOULDER_RIGHT));
-		System.out.println("Right elbow: " + skeletons[i].get3DJointX(Skeleton.ELBOW_RIGHT) + "," + skeletons[i].get3DJointY(Skeleton.ELBOW_RIGHT) + "," + skeletons[i].get3DJointZ(Skeleton.ELBOW_RIGHT));
-		System.out.println("Right wrist: " + skeletons[i].get3DJointX(Skeleton.WRIST_RIGHT) + "," + skeletons[i].get3DJointY(Skeleton.WRIST_RIGHT) + "," + skeletons[i].get3DJointZ(Skeleton.WRIST_RIGHT));
-		System.out.println(i + "----------------------------------------");
-		*/
+		 * System.out.println("Right shoulder: " + skeletons[i].get3DJointX(Skeleton.SHOULDER_RIGHT) + "," + skeletons[i].get3DJointY(Skeleton.SHOULDER_RIGHT) + "," + skeletons[i].get3DJointZ(Skeleton.SHOULDER_RIGHT));
+		 * System.out.println("Right elbow: " + skeletons[i].get3DJointX(Skeleton.ELBOW_RIGHT) + "," + skeletons[i].get3DJointY(Skeleton.ELBOW_RIGHT) + "," + skeletons[i].get3DJointZ(Skeleton.ELBOW_RIGHT));
+		 * System.out.println("Right wrist: " + skeletons[i].get3DJointX(Skeleton.WRIST_RIGHT) + "," + skeletons[i].get3DJointY(Skeleton.WRIST_RIGHT) + "," + skeletons[i].get3DJointZ(Skeleton.WRIST_RIGHT));
+		 * System.out.println(i + "----------------------------------------");
+		 */
 		
 		//seems like skeletal data is only captured on a 6 frame interval, and that i is set as a constant between 0 and 5
 	
@@ -26,7 +27,8 @@ public class KinectReader {
 		 * 
 		 * squares are those distances squared, respectively
 		 * 
-		 * Formula to calculate angle between three points can be found here: http://stackoverflow.com/questions/1211212/how-to-calculate-an-angle-from-three-points
+		 * Formula to calculate angle between three points can be found here: 
+		 * http://stackoverflow.com/questions/1211212/how-to-calculate-an-angle-from-three-points
 		 * Although, I did derive it myself.
 		 */
 		
